7ce430b69597c089f1131d91a4b81ed49e052f6e
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-04-14 14:51:58 -0700

    dynamic

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,34 +1,11 @@
 import "./globals.css";
 import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { headers, cookies } from "next/headers";
-// import StripeProvider from "@/components/providers/stripe-provider";
-// import SupabaseProvider from "@/components/providers/supabase-provider";
-// import Footer from "@/components/Footer";
-// import NavBar from "@/components/Navbar";
-// import MobileNav from "@/components/MobileNav";
-import dynamic from "next/dynamic";
-
-const StripeProvider = dynamic(
-    () => import("@/components/providers/stripe-provider"),
-    {
-        loading: () => <p>Loading...</p>,
-    }
-);
-const SupabaseProvider = dynamic(
-    () => import("@/components/providers/supabase-provider"),
-    {
-        loading: () => <p>Loading...</p>,
-    }
-);
-const MobileNav = dynamic(() => import("@/components/MobileNav"), {
-    loading: () => <p>Loading...</p>,
-});
-const NavBar = dynamic(() => import("@/components/Navbar"), {
-    loading: () => <p>Loading...</p>,
-});
-const Footer = dynamic(() => import("@/components/Footer"), {
-    loading: () => <p>Loading...</p>,
-});
+import StripeProvider from "@/components/providers/stripe-provider";
+import SupabaseProvider from "@/components/providers/supabase-provider";
+import Footer from "@/components/Footer";
+import NavBar from "@/components/Navbar";
+import MobileNav from "@/components/MobileNav";
 
 export const metadata = {
     title: "SupaCommerce",
